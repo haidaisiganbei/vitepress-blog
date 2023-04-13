25bd7581aaf832dc57c589a6caa75e3a7b9f7a8d
lin yijie <yijie_lin@fzzixun.com>
2023-04-13 15:12:45 +0800

    用chatgpt生成文章

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -8,19 +8,85 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: '前端', link: '/front/html' },
+      { text: '后端', link: '/back/java' },
+      { text: '关于我', link: '/about' }
     ],
 
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
-    ],
-
+    sidebar: {
+      '/front/': [
+        {
+          text: '前端',
+          items: [
+            { text: 'JavaScript', link: '/front/javascript' },
+            { text: 'CSS', link: '/front/css' },
+            { text: 'HTML', link: '/front/html' },
+            { text: 'Vue', link: '/front/vue' },
+            { text: 'React', link: '/front/react' },
+            { text: 'Node', link: '/front/node' },
+            { text: 'Webpack', link: '/front/webpack' },
+            { text: 'Vite', link: '/front/vite' },
+            { text: 'TypeScript', link: '/front/typescript' },
+            { text: 'Git', link: '/front/git' },
+            { text: 'HTTP', link: '/front/http' },
+            { text: '浏览器', link: '/front/browser' },
+            // { text: '算法', link: '/front/algorithm' },
+            // { text: '数据结构', link: '/front/data-structure' },
+            // { text: '设计模式', link: '/front/design-pattern' },
+            // { text: '计算机网络', link: '/front/computer-network' },
+            // { text: '操作系统', link: '/front/operating-system' },
+            // { text: '数据库', link: '/front/database' },
+            // { text: '编译原理', link: '/front/compiler' },
+            // { text: '计算机组成原理', link: '/front/computer-organization' },
+            // { text: '计算机体系结构', link: '/front/computer-architecture' },
+            // { text: '计算机图形学', link: '/front/computer-graphics' },
+            // { text: '计算机视觉', link: '/front/computer-vision' },
+            // { text: '人工智能', link: '/front/artificial-intelligence' },
+            // { text: '机器学习', link: '/front/machine-learning' },
+            // { text: '深度学习', link: '/front/deep-learning' },
+            // { text: '自然语言处理', link: '/front/natural-language-processing' },
+            // { text: '计算机安全', link: '/front/computer-security' },
+            // { text: '区块链', link: '/front/blockchain' },
+            // { text: '微服务', link: '/front/microservice' },
+            // { text: '云计算', link: '/front/cloud-computing' },
+            // { text: '大数据', link: '/front/big-data' },
+            // { text: '移动端', link: '/front/mobile' },
+            { text: '前端工程化', link: '/front/front-end-engineering' },
+            { text: '性能优化', link: '/front/performance-optimization' },
+            { text: '测试', link: '/front/test' },
+            { text: '工具', link: '/front/tool' },
+            { text: '面试', link: '/front/interview' },
+            { text: '其他', link: '/front/other' },
+          ]
+        }
+      ],
+      '/back/': [
+        {
+          text: '后端',
+          items: [
+            { text: 'Java', link: '/back/java' },
+            { text: 'Spring', link: '/back/spring' },
+            { text: 'Spring Boot', link: '/back/spring-boot' },
+            { text: 'Spring Cloud', link: '/back/spring-cloud' },
+            { text: 'Spring Security', link: '/back/spring-security' },
+            { text: 'Spring Data', link: '/back/spring-data' },
+            { text: 'Spring Batch', link: '/back/spring-batch' },
+            { text: 'Spring Integration', link: '/back/spring-integration' },
+            { text: 'Spring WebFlux', link: '/back/spring-webflux' },
+            { text: 'Spring Web MVC', link: '/back/spring-web-mvc' },
+            { text: 'Spring WebSocket', link: '/back/spring-websocket' },
+            { text: 'Spring Boot Admin', link: '/back/spring-boot-admin' },
+            { text: 'Spring Boot Actuator', link: '/back/spring-boot-actuator' },
+            { text: 'Spring Boot Data', link: '/back/spring-boot-data' },
+            { text: 'Spring Boot Security', link: '/back/spring-boot-security' },
+            { text: 'Spring Boot Web', link: '/back/spring-boot-web' },
+            { text: 'Spring Boot Test', link: '/back/spring-boot-test' },
+            { text: 'Spring Boot DevTools', link: '/back/spring-boot-devtools' },
+            { text: 'Spring Boot Starter', link: '/back/spring-boot-starter' },
+          ]
+        },
+      ],
+    },
     socialLinks: [
       { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
     ]
---
+++ docs/front/javascript.md
@@ -0,0 +1,39 @@
+---
+sidebar: auto
+---
+
+# JavaScript 知识点整理
+
+## 基础语法
+
+- 数据类型：JavaScript 的基本数据类型包括数字、字符串、布尔值、null 和 undefined，以及引用类型对象。
+- 变量：在 JavaScript 中使用 var、let 和 const 定义变量。
+- 运算符：JavaScript 支持算术运算、比较运算、逻辑运算等常见运算符。
+- 控制语句：JavaScript 提供了 if-else、switch、for、while 等常用的控制语句。
+- 函数：JavaScript 中的函数可以作为变量使用，可以通过函数表达式和函数声明两种方式定义函数。
+- 对象和数组：JavaScript 中的对象和数组是两种常用的数据结构，可以通过对象字面量和数组字面量创建。
+
+## 高级特性
+
+- 闭包：函数可以访问自身作用域以外的变量，这种特性称为闭包。
+- 原型和继承：JavaScript 中的对象可以通过原型链实现继承。
+- 异步编程：JavaScript 中的异步编程可以通过回调函数、Promise 和 async/await 等方式实现。
+- ES6+ 新特性：ES6+ 引入了 let 和 const 定义变量、箭头函数、解构赋值、模板字符串、类和模块等新特性。
+
+## 应用场景
+
+- 前端开发：JavaScript 是前端开发中不可或缺的语言，可以实现网页交互、动画效果等功能。
+- 后端开发：Node.js 让 JavaScript 可以在服务器端运行，成为了后端开发中的一种常用语言。
+- 桌面应用开发：Electron 等框架让 JavaScript 可以用于开发桌面应用程序。
+- 移动应用开发：React Native 等框架让 JavaScript 可以用于开发移动应用程序。
+
+## 工具和框架
+
+- jQuery：一个流行的 JavaScript 库，封装了常见的 DOM 操作和 Ajax 请求等功能。
+- React：一个流行的 JavaScript 框架，可以用于构建复杂的 UI 界面。
+- Vue：一个流行的 JavaScript 框架，也可以用于构建复杂的 UI 界面。
+- Angular：一个流行的 JavaScript 框架，提供了完整的 MVC 框架和依赖注入等功能。
+- Node.js：一个流行的 JavaScript 运行时，可以在服务器端运行 JavaScript 代码。
+- Express：一个流行的 Node.js 框架，可以快速构建 Web 应用程序。
+- Webpack：一个流行的 JavaScript 打包工具，可以将多个 JavaScript 文件打包成一个文件。
+- Babel：一个流行的 JavaScript 编译器，可以将 ES6+ 代码转
